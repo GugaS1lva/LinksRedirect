3d17d340ee18dd5e9a327ec2bc55adfe1b1f6900
Gustavo Silva <contatogustavosilva017@outlook.com>
2025-02-28 04:15:24 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-// src/app/page.tsx
 import { redirect } from 'next/navigation';
 import getLatestVideoUrl from '../lib/youtube';
 
@@ -8,11 +7,8 @@ export default async function HomePage() {
     videoUrl = await getLatestVideoUrl();
   } catch (error) {
     console.error("Erro ao obter a URL do vídeo:", error);
-    // Em caso de erro, define o fallback para o canal
     videoUrl = "https://www.youtube.com/@gugas1lvadev";
   }
   
-  // Essa chamada dispara o redirect e lança a exceção NEXT_REDIRECT, que é tratada internamente pelo Next.js.
   redirect(videoUrl);
-  // Código após o redirect() nunca será executado.
 }
